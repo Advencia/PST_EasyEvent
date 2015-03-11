67f09930f32fbebb3cee2e3114d87c0a34682213
KAYA Eray <eray_kaya@hotmail.fr>
2015-03-11 12:38:17 +0100

    login admin

---
+++ test_login_gala/src/com/example/mysqltest/ReadComments.java
@@ -0,0 +1,14 @@
+package com.example.mysqltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadComments extends Activity{
+
+	 @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.read_comments);
+	}
+}
