28108b1be15dc1edd44a259b1b417f48ab57ad40
mwdoan <mdoan@et.esiea.fr>
2015-03-11 22:18:32 +0100

    removed gala_esay_event_bis

--- Gala_Easy_Event_bis/src/com/example/gala_easy_event/FetchDataListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gala_easy_event;
-
-import java.util.List;
-
-public interface FetchDataListener {
-    public void onFetchComplete(List<Etudiant> data);
-    public void onFetchFailure(String msg);
-}
-
